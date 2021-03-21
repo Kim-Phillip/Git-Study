6f65afaf4aaad6f8a8af03c479144761620dbd5f
phillip <smkim961211@gmail.com>
2021-03-21 22:41:05 +0900

    Add divide function

--- calculator.py
+++ calculator.py
@@ -1,15 +1,12 @@
-# 기본계산기
+## 기본계산기
 def add(a, b):
 	return a+b
 
 def subtract(a, b):
 	return a-b
 
-<<<<<<< HEAD
-=======
 def multiply(a, b):
 	return a*b
 
->>>>>>> main
 def divide(a, b):
 	return a/b
