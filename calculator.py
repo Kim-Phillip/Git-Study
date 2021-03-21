c0aad793e3995d0b2157d83771aefc4f741d81a5
phillip <smkim961211@gmail.com>
2021-03-21 22:34:15 +0900

    Add divide function

--- calculator.py
+++ calculator.py
@@ -6,11 +6,9 @@ def subtract(a, b):
 	return a-b
 
 def multiply(a, b):
-<<<<<<< HEAD
 	return a*b
 
 def divide(a, b):
 	return a/b
-=======
-	return a*b
->>>>>>> 964bce50d2e430c9dc7e9e227e8f321e12b79750
+
+
